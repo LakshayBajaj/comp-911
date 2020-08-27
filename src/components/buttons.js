54c569c072ec30ff877ace721bee3078948aaaf5
Lakshay <lakshaybajaj77459@gmail.com>
2020-08-27 17:17:43 +0530

    alignment

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -6,16 +6,15 @@ class Row extends React.Component {
     render() {
         return (
             <div className="text-center justify-content-center row">
-                
-                    <button type="button" className="col-4 btn btn-light pl-2 pr-2 pt-2 pb-2 mt-1 mb-1 mr-1 ml-1"><h6><i class="mr-1 fas fa-helicopter"></i>Air Doctor</h6></button>
-                
-                
-                    <button type="button" className="col-3 btn btn-light pl-2 pr-2 pt-2 pb-2 mt-1 mb-1 ml-0 mr-0 mr-xs-3 ml-xs-3"><h6>Santa Rose Police</h6></button>
-                
-                
-
-                    <button type= "button" className="col-4 btn btn-light pl-2 pr-2 pt-2 pb-2 mt-1 mb-1 mr-1 ml-1"><h6><i className="mr-1 fas fa-ambulance"></i>Ambulance</h6></button>
-                
+                <div className="col-4" >
+                    <button type="button" className="container-fluid btn btn-light pl-2 pr-2 pt-2 pb-2 mt-1 mb-1 mr-1 ml-1"><h6><i class="mr-2 fas fa-helicopter"></i>Air Doctor</h6></button>
+                </div>
+                <div className="col-4" >
+                    <button type="button" className="container-fluid btn btn-light pl-2 pr-2 pt-2 pb-2 mt-1 mb-1 ml-0 mr-0 mr-xs-3 ml-xs-3"><h6>Santa Rose Police</h6></button>
+                </div>
+                <div className="col-4" >
+                    <button type="button" className="container-fluid btn btn-light pl-2 pr-2 pt-2 pb-2 mt-1 mb-1 mr-1 ml-1"><h6><i className="mr-2 fas fa-ambulance"></i>Ambulance</h6></button>
+                </div>
 
 
             </div>
